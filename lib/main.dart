c449c2f2350b9a54886584e4ca72dcd28498c75a
Ailton <f.futuretechcode@gmail.comm>
2020-06-03 09:56:51 -0400

    Mudança na estrutrura

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:romantico/home_page.dart';
+import 'package:romantico/screens/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+       
       ),
       home: HomePage (),
     );
